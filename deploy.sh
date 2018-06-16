7933960a51cba71b611277cbe07e795ed0924630
Richard <richard.dimalanta@gmail.com>
2018-06-16 10:41:34 +1000

    build test 4

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Test Deployment using Travis"
 echo $TRAVIS_BRANCH
-if [ $TRAVIS_BRANCH == 'dev' ]; then
-  # Staging env variables
-  npm install -g serverless
-  sls deploy
-elif [ $TRAVIS_BRANCH == 'master' ]; then
-  # Prod env variables
-  npm install -g serverless
-  sls deploy --stage production
-else
-  exit 0
-fi
+npm install -g serverless
+sls deploy
